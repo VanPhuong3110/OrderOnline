5acf0bb02076f1c5cc9db862abad1bf400c0cd23
VanPhuong3110 <vanphuongbs@gmail.com>
2024-01-03 23:13:06 +0700

    new commit

---
+++ about.php
@@ -0,0 +1,20 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    <p>Chúng tôi duy trì một hệ thống hỗ trợ người dùng order đồ ăn tốt nhất.</p>
+    <p>Với phương châm luôn đặt chất lượng dịch vụ lên hàng đầu, đảm bảo bạn luôn có những trải nghiệm tốt nhất.Đến thời điểm hiện tại, với hàng trăm ngàn địa điểm và hàng trăm ngàn bình luận, hình ảnh tại Việt Nam, ở hầu hết các tỉnh thành. CHúng tôi dược tin tin tưởng là cách dễ nhất để bạn có thể tìm kiếm và lựa chọn địa điểm tốt nhất cho mình và bạn bè.</p>            
+    <p>Xin chân thành cảm ơn các quý khách hàng trên toàn quốc đã luôn tin tưởng và lựa chọn cho chúng tôi.</p>
+    </div>
+        </section>
